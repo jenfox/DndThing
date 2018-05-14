c8c0f2fc5e22dd11feb9e575200c005269333d58
Justin Joseph <josephjustn@gmail.com>
2018-05-14 12:10:03 -0400

    formatting

--- dnd/src/app/character/services/get-spells.service.ts
+++ dnd/src/app/character/services/get-spells.service.ts
@@ -7,8 +7,7 @@ export class GetSpellsService {
 
   baseURL = 'http://dnd5eapi.co/api/';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) { }
 
   getClassSpells(className: string) {
     return this.http.get(this.baseURL + 'spells/' + className + '/');
